96210d7c85cbbe79850b711bbe4f2f155812d429
sahanchathurangaherath <sahanchathurangaherath@gmail.com>
2026-08-02 20:54:57 +0530

    Add AccessOneApplication main class

---
+++ backend/src/main/java/lk/AccessOne/AccessOneApplication.java
@@ -0,0 +1,12 @@
+package lk.AccessOne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccessOneApplication.class, args);
+    }
+}
